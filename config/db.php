2d9699d56ed231f171746c2a50026645017a65f1
AyalaTon <88519142+AyalaTon@users.noreply.github.com>
2024-01-29 15:09:21 -0300

    IP instead localhost

--- config/db.php
+++ config/db.php
@@ -2,11 +2,11 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=milton_gastos',
+    'dsn' => 'mysql:host=179.27.153.62;dbname=milton_gastos',
     'username' => 'milton_gastos',
     'password' => 'hu?qwe64.@#&12jfa',
     'charset' => 'utf8',
-    
+
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
