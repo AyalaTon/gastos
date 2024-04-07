f5afa99121827d790540d450c1318bd530de3271
AyalaTon <88519142+AyalaTon@users.noreply.github.com>
2024-04-07 00:41:19 -0300

    signIn added to routes_users

--- src/routes/routes_users.php
+++ src/routes/routes_users.php
@@ -10,5 +10,14 @@ return function (App $app){
 	$container = $app->getContainer();
 	$userController = new ctr_user();
 
+	$app->post('/signIn', function(Request $request, Response $response) use ($container, $userController){
+		$responseCurrentSession = $userController->validateCurrentSession();
+		if($responseCurrentSession->result != 2){ // No hay sesion activa
+			$data = $request->getParams();
+			$user = $data['user'];
+			$password = $data['password'];
+			return json_encode($userController->signIn($user, sha1($password)));
+		}else return json_encode($responseCurrentSession);
+	});
 }
 ?>
